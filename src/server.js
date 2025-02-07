10a42dee3d03efe38ae9f29f7f6c6fec60b36083
appsy-gc <167824281+appsy-gc@users.noreply.github.com>
2025-02-07 16:58:17 +1000

    feat: added some error handling

--- src/server.js
+++ src/server.js
@@ -10,6 +10,26 @@ app.get('/', (request, response) => {
     })
 })
 
+// Wildcard * means "match any route"
+// Put at the end of route declarations
+// to catch anything that does not match an earlier route
+app.get('*', (request, response) => {
+    console.log('User tried to visit ' + request.path)
+    response.status(404).json({
+        message: 'Page not found. ',
+        attemptedPath: request.path
+    })
+})
+
+app.use((error, request, response, next) => {
+    console.log('Error occurred in the server.')
+    console.log(JSON.stringify(error))
+    response.json({
+        errors: request.body.errors,
+        message: error.message
+    })
+})
+
 module.exports = {
     app
 }
