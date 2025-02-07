2673dd3986dadf1e1a7657367499844c67bf1c4b
appsy-gc <167824281+appsy-gc@users.noreply.github.com>
2025-02-07 16:50:00 +1000

    feat: port controlled by env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 // Import configured server and run
+
+const dotenv = require('dotenv')
+dotenv.config()
+
 const { app } = require('./server')
 
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
